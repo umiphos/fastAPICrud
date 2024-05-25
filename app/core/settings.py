895b9245f19d46186353c713b666470e1ea316a5
umiphos <umiphos@gmail.com>
2024-05-24 21:23:50 -0600

    [ADD]: setting to standarize the use of variables

---
+++ app/core/settings.py
@@ -0,0 +1,7 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    ENV: str = "develop"
+
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
 from fastapi import FastAPI
+from .core.settings import settings
 
-app = FastAPI()
+
+app = FastAPI(
+    title="Curricular control",
+    docs_url='/api/v1/scholar_control/docs' if settings.ENV in ("develop") else None
+)
